132f98a3b8449604be9a925990ed745f1abe2b9b
Lutha <luthamahtin@gmail.com>
2014-07-23 14:24:22 -0500

    add player shot boundary

---
+++ Assets/Scripts/BoundaryBullets.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundaryBullets : MonoBehaviour {
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.CompareTag ("Bullet"))
+			Destroy (other.gameObject);
+	}
+}
